b156188a47a0b391457fa82cdf2d7036a7f96773
rish-dev-plunes <rishav.upadhayay@plunes.com>
2020-06-25 16:14:05 +0530

    auth header changes

--- src/components/functional/AuthHeader.js
+++ src/components/functional/AuthHeader.js
@@ -4,51 +4,54 @@ import { Link } from "react-router-dom"
 const AuthHeader = (props) =>{
   console.log(window.location,"window.location")
     return(
-     <div className="header">
-       <div>
-        
-<nav className="navbar custom_ha navbar-expand-md custom-navbar sigunup_hedr">
-        <div className="container">
-            <Link className="navbar-brand logo_size col-lg-3 col-md-3 col-6" to="/">
-                <img src="/logo.jpg" className="lgo_sigun" />
+      <nav className="navbar custom_ha navbar-expand-md custom-navbar ">
+      <div className="header_wrapper_rish oversize_wrap_rish display_flex">
+        <div className="header_wrapper_logo_child_rish ">
+          <div style={{position:'relative',width:'100%', height:'100%'}} className='center_align_rish' >
+            <Link to="/">
+              <img src="/logo.jpg" className="logo_rish cursor-pointer" />
             </Link>
-           <button className="navbar-toggler navbar-toggler-right custom-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-            <i className="fa fa-bars" aria-hidden="true"></i>
-           </button>
-             <div className="collapse navbar-collapse col-lg-9 col-12 col-md-9 back_color_mo" id="collapsibleNavbar">
-                 <ul className="navbar-nav ml-auto Three_butn dr_lis_mobile">
-                   <li className="nav-item">
-                    <div className="dropdown drp_part">
-                <button type="button" className="btn dropdown-toggle cus_bu" data-toggle="dropdown" >
-                  Download the App
-                    </button>
-                  <ul className="dropdown-menu" role="tree" aria-expanded="true" aria-hidden="false">
-                  <li className="icons_pdi"><a href="https://apps.apple.com/us/app/plunes/id1463747553/" title="app store">
-                    <img className="OIS" src="/OIS.jpg" alt=".." />
-                    <span className="ios-img-header">iOS</span></a>
-                  </li>
-                    <hr width="70%"></hr>
-                  <li className="icons_pdi">
-                    <a href="https://play.google.com/store/apps/details?id=com.plunes&hl=en_IN/" title="play store">
-                      <img className="Andgre" src="/Andgre.jpg" alt=".." />
-                      <span className="ios-img-header">Android</span></a>
-                  </li>
-                </ul>
-              </div>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/signin" className={`${window.location.pathname==='/'?'active':''} nav-link sig_lgn a `} >Login</Link>
-                  </li>
-                   <li className="nav-item">
-                     <Link to="/signup"  className={`${window.location.pathname==='/signup'?'active':''} nav-link signup_lgu`}  href="/signup">Signup</Link>
-                   </li>  
-                 </ul>
-             </div>  
+          </div>
+        
         </div>
+      <div className="header_wrapper_links_child_rish mobile_header_links">
+         <button className="navbar-toggler navbar-toggler-right custom-toggler" type="button" >
+          <i className="fa fa-bars" aria-hidden="true"></i>
+         </button>
+           <div className="collapse navbar-collapse" id="collapsibleNavbar">
+              <ul className="navbar-nav ml-auto Three_butn dr_lis_mobile">
+              <li className="nav-item header_li_rish">
+                    <Link to="/signup"
+                    > <text style={{color:window.location.pathname==='/signup'?'#01D35A !important':'inherit'}}  className={`${window.location.pathname==='/signup'?'green_text_rish':'header_link_text'} `} >Signup</text></Link>
+                </li> 
+               <li className="nav-item header_li_rish">
+                    <Link to="/signin"
+                    > <text style={{color:window.location.pathname==='/signin'?'#01D35A !important':'inherit'}}   className={`${window.location.pathname==='/signin'?'green_text_rish':'header_link_text'} `} >Login</text></Link>
+                </li>  
+                <li className="nav-item header_li_rish">
+                    <div className="dropdown drp_part">
+                       <text type="button"  className="header_link_text"  data-toggle="dropdown" >
+                          Download the App <img src="/Small-Arrow-Down.svg" style={{marginLeft:'.8rem', height:'0.6rem', width:'auto'}}  />
+                       </text>
+                       <ul className="dropdown-menu" role="tree" aria-expanded="true" aria-hidden="false">
+                           <li className="icons_pdi"><a href="https://apps.apple.com/us/app/plunes/id1463747553/" title="app store">
+                               <img className="OIS" src="/OIS.jpg" alt=".." />
+                               <span className="ios-img-header">iOS</span></a>
+                            </li>
+                            <hr width="70%"></hr>
+                            <li className="icons_pdi">
+                               <a href="https://play.google.com/store/apps/details?id=com.plunes&hl=en_IN/" title="play store">
+                               <img className="Andgre" src="/Andgre.jpg" alt=".." />
+                               <span className="ios-img-header">Android</span></a>
+                            </li>
+                        </ul>
+                      </div>
+                 </li>
+               </ul>
+           </div>  
+      </div>
+    </div>
 </nav>
-
-</div>
-</div>
             
     )
 }
