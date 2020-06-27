d7806b0a266b0e9e4b1804a24be3cd2a918a1006
rish-dev-plunes <rishav.upadhayay@plunes.com>
2020-06-27 18:26:48 +0530

    admin_route change

--- src/HOC/admin_route.js
+++ src/HOC/admin_route.js
@@ -2,7 +2,7 @@ import React , { Component } from 'react'
 import { Redirect } from "react-router-dom"
 
  const admin_route = ({authObject, ...rest}) => (getComponent) => {
-                if(!!(authObject().isAdmin)){
+                if((!!(authObject().isAdmin)) || (((!authObject().isAdmin) && (!authObject().isCenter)))){
                     return  getComponent()
                   }else{
                     return  <Redirect to="/dashboard" />
