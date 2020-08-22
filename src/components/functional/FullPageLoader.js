541009aebc4f0bde5c5d56ccfc56e4bcc98dc240
rishav upadhayay <rishav.0727@gmail.com>
2020-08-22 16:10:12 +0530

    loader change

--- src/components/functional/FullPageLoader.js
+++ src/components/functional/FullPageLoader.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const FullPageLoader = () => {
     return (
-        <div className="loading-full_page" delay-hide="50000"></div>
+        <div className="loading-full_page" delay-hide="50000">
+          <div >
+            <img className="fullpage_loader_icon " src="/icon/logo.svg" />
+         </div>
+        </div>
     )
 }
 
