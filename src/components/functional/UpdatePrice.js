1abc35526eaac51f7b87b45973a731bbce5acead
rishav upadhayay <rishav.0727@gmail.com>
2020-08-28 12:42:07 +0530

    heading change in update price

--- src/components/functional/UpdatePrice.js
+++ src/components/functional/UpdatePrice.js
@@ -7,7 +7,7 @@ const UploadPrice = (props) => {
     return (
         <React.Fragment>
                 <div className ='modal-wrapper-small_rish'>
-             <div style={{fontWeight:'600'}} className="modal-heading_ris margin_top_small_rish update_price_heading">  { `Update price in Catalogue`}</div>
+             <div style={{fontWeight:'600'}} className="modal-heading_ris margin_top_small_rish update_price_heading">  { `Update price`}</div>
                  <div style={{height:'5rem'}}>
                     <span className="modal_content_description center_align_rish margin_top_small_rish">
                         {/* { `Are you sure you want remove ${[...props.specialities].filter(item=>!!(item.value === props.selected_speciality))[0].name} from catalogue?`} */}
@@ -16,10 +16,10 @@ const UploadPrice = (props) => {
 
                          {props.update_procedure_loading_flag && <LoaderComponent />}
                             <span className="flex_parent_child text-center">
-                                <Button id="make_transaction_button" className="modal_button" onClick={()=>props.update_procedure({...props.update_procedure_obj})}  >Update, For this facility </Button>
+                                <Button id="make_transaction_button" className="modal_button" onClick={()=>props.update_procedure({...props.update_procedure_obj})}  >Update this Profile</Button>
                             </span>
                             <span className="flex_parent_child text-center">
-                                <Button id="cancel_button" className="modal_button" onClick={()=>console.log()}  >Update, For all the facilities </Button>
+                                <Button id="cancel_button" className="modal_button" onClick={()=>console.log()}  >Update all centres</Button>
                             </span>
                      </div>
                  </div>
