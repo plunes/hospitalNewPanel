7e47482a9717304339fdc84646075763e2127145
rishav <rishav.0727@gmail.com>
2020-12-14 14:18:03 +0530

    Cancellation Requested bookingStatus added

--- src/components/DashboardComponent/AppointmentComponent.js
+++ src/components/DashboardComponent/AppointmentComponent.js
@@ -212,7 +212,7 @@ class AppointmentComponent extends Component {
                         confirmed_bookings.push(data)
                        }
                       
-                   }else if(data.bookingStatus==="Cancelled"){
+                   }else if(data.bookingStatus==="Cancelled" || data.bookingStatus==="Cancellation Requested"){
                     if(!!redirect_index){
                         if(appointments_arr[redirect_index]._id !== data._id){
                             console.log("cancelled_bookings")
